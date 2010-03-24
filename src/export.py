f553d92affbfe53a1a1319a89fa74b41d20f036e
Chad Daelhousen <devel@sapphirepaw.org>
2010-03-23 21:20:57 -0400

    Add --name option to export.py.

--- src/export.py
+++ src/export.py
@@ -3,7 +3,11 @@
 
 def parse_args (argv):
     p = optparse.OptionParser(prog=os.path.basename(argv[0]),
-                              usage='%prog profile [filename]')
+                              usage='%prog [options] profile [filename]')
+
+    p.add_option("-n", "--name", dest="name", metavar="NAME",
+                 help="Set the profile name to NAME in the exported file")
+
     return p.parse_args(argv[1:])
 
 def main (argv=None, profile=None, filename=None):
@@ -15,10 +19,11 @@ def main (argv=None, profile=None, filename=None):
             argv.append(filename)
 
     (opts, args) = parse_args(argv)
+    real_name = opts.name if opts.name else args[0]
     if len(args) < 1:
         usage("Missing profile name.")
     elif len(args) == 1:
-        args.append("%s.zip" % args[0])
+        args.append("%s.zip" % real_name)
     elif len(args) > 2:
         usage("Too many arguments.")
 
@@ -27,6 +32,7 @@ def main (argv=None, profile=None, filename=None):
     io = GnomeTerminalIO()
     try:
         dst = io.read_profile(profile_name)
+        dst.name = real_name
     except:
         p_err("The theme '%s' does not exist." % profile_name)
         sys.exit(1)
@@ -39,7 +45,9 @@ def main (argv=None, profile=None, filename=None):
         p_err("\t%s" % e)
         sys.exit(1)
 
-    print "Exported theme '%s' to %s." % (profile_name, filename)
+    print "Exported theme '%s' as '%s' to %s." % (profile_name,
+                                                  real_name,
+                                                  filename)
 
 
 if __name__ == '__main__':
