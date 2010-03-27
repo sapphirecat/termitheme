16f9f3a4f09404a496c57b89c28192c5240fc378
Chad Daelhousen <devel@sapphirepaw.org>
2010-03-26 20:45:24 -0400

    Create TerminalIOBase and flesh out platform-detection code.

--- src/libtermitheme.py
+++ src/libtermitheme.py
@@ -36,7 +36,269 @@ _term_support = {
 
 
 
-#{{{1 GConf value boxing/unboxing
+#{{{1 Color conversion
+
+_c24_re = re.compile('^#?' + (3 * '([0-9a-f]{2})') + '$', re.I)
+_c48_re = re.compile('^#?' + (3 * '([0-9a-f]{4})') + '$', re.I)
+
+def color24 (c48): #{{{
+    """Convert a 48-bit color to the closest possible 24-bit color."""
+
+    m = _c48_re.match(c48)
+    if m is None:
+        raise ValueError("Invalid 48-bit color '%s'" % c48)
+    c24 = '#'
+    for hexval in m.groups():
+        byte = round(int(hexval, 16)/256, 0)
+        if byte > 255:
+            byte = 255
+        elif byte < 0:
+            byte = 0
+        c24 += '%02x' % byte
+    return c24
+    #}}}
+
+def color48 (c24): #{{{
+    """Convert a 24-bit color to a 48-bit color."""
+
+    c24 = c24.lstrip('#')
+    if len(c24) != 6:
+        raise ValueError("24-bit color '%s' has unexpected length" % c24)
+    c48 = []
+    for b in range(3):
+        v = c24[2*b:2*b+2]
+        c48.append(v + v)
+    return '#' + (''.join(c48))
+    #}}}
+
+def is_color24 (c):
+    if isinstance(c, basestring) and _c24_re.match(c):
+        return True
+    return False
+
+def is_color48 (c):
+    if isinstance(c, basestring) and _c48_re.match(c):
+        return True
+    return False
+
+#}}}1
+
+
+#{{{1 Theme file IO
+#
+# The file format is intentionally simple: a zip file containing a theme.ini
+# file, whose keys/values are exactly what TerminalProfile is.
+#
+# theme.ini, of course, is just an INI file like so...
+# [Termitheme1]
+# key=value
+# key=value
+#
+# This way, everything can be handled with zipfile and ConfigParser; and
+# when we get to figuring out where to unpack a background-image, the image
+# can be neatly added to the theme zip without changing the overall format.
+#
+# As such, this version is designed not to care about extra files in the zip
+# archive.
+#
+
+class ThemeFile (object):
+    filename = None
+
+    _autotype_defs = [
+        (re.compile(r'^t', re.I), lambda x: True),
+        (re.compile(r'^f', re.I), lambda x: False),
+        (re.compile(r'^\d+$'), lambda x: int(x, 10)),
+        (re.compile(r'^\d*\.\d+(?:e\d+)?$', re.I), lambda x: float(x)),
+    ]
+
+    def __init__ (self, filename, profile_class=None):
+        self.filename = filename
+        if not profile_class:
+            profile_class = TerminalProfile
+        self._profile_ctor = profile_class
+
+    def read_open (self):
+        """Return a ConfigParser associated with the theme file."""
+
+        # Open zipfile and validate expected theme.ini file found
+        zf = zipfile.ZipFile(self.filename, 'r') # can raise IOError
+        # Python2.5 doesn't have ZipFile.open(), and ConfigParser doesn't
+        # have any way to read directly from a string.
+        fp = StringIO.StringIO(zf.read('theme.ini')) # can raise KeyError
+        zf.close()
+
+        cp = ConfigParser.RawConfigParser()
+        cp.readfp(fp)
+        return cp
+
+    def read_profile (self, parser):
+        profile_class = self._profile_ctor
+        try:
+            name = parser.get('Termitheme1', 'name')
+            p = profile_class(name)
+        except ConfigParser.NoOptionError:
+            raise KeyError("Theme file does not have a 'name' key.")
+
+        for k, v in parser.items('Termitheme1'):
+            if k == 'name': # special key
+                continue
+
+            # the autotyper makes this O(M*N) but there's no other way to
+            # get booleans into gconf as real booleans
+            p[k] = self._autotype(v)
+
+        return p
+
+    def read (self):
+        """Read the theme file in self.filename and return a profile."""
+        return self.read_profile(self.read_open())
+
+    def write (self, profile):
+        """Write the profile into self.filename."""
+
+        lines = ["[Termitheme1]\n", "name = %s\n" % profile.name]
+        for k,v in profile.items():
+            lines.append("%s = %s\n" % (k, v))
+
+        info = zipfile.ZipInfo()
+        info.filename = 'theme.ini'
+        # whoever designed this api is ridiculous
+        now = datetime.datetime.fromtimestamp(time.time())
+        info.date_time = (now.year, now.month, now.day,
+                          now.hour, now.minute, now.second)
+        info.compress_type = zipfile.ZIP_DEFLATED
+        info.external_attr = 0644 << 16L # thank you bug 3394 (swarren)
+
+        if os.path.exists(self.filename):
+            raise ValueError("File '%s' exists." % self.filename)
+        zf = zipfile.ZipFile(self.filename, 'w')
+        zf.writestr(info, ''.join(lines))
+        zf.close()
+
+    def _autotype (self, val):
+        for re, fn in self._autotype_defs:
+            if re.match(val):
+                return fn(val)
+        return val
+
+#}}}
+
+
+#{{{ Generic terminal profile data structure
+# This is smarter than a dict for future support of PuTTY/Konsole themes.
+
+class TerminalProfile (dict):
+    """Generic terminal theme."""
+
+    PROFILE_KEY_NAMES = ["color%d" % i for i in range(16)]
+    PROFILE_KEY_NAMES.extend([
+        # common (plus color0 through color15)
+        'bgcolor',
+        'cursor_shape',
+        'font',
+        'fgcolor',
+        'fgbold',
+        # gnome-terminal
+        'force_font',
+        'allow_bold',
+        'use_fgbold',
+        # PuTTY's cursor color, etc.? Konsole?
+    ])
+
+    name = None
+
+    def __init__ (self, name):
+        self._valid_keys = set(self.PROFILE_KEY_NAMES)
+        self.name = name
+        self._io_data = {}
+
+    def is_valid_key (self, k):
+        """Return True if k is a TerminalProfile key."""
+        return k in self._valid_keys
+
+    @contextmanager
+    def ioslave (self, slave_name):
+        """Give a reference to a slave-private dict to a block.
+        
+        Example: with profile.ioslave("konsole") as d: d['foo']='bar'"""
+        if slave_name not in self._io_data:
+            self._io_data[slave_name] = {}
+        yield self._io_data[slave_name]
+
+    #{{{ Dictionary interface
+    def update (self, other):
+        """Copy other's dictionary keys and ioslave data into self."""
+
+        if not isinstance(other, TerminalProfile):
+            raise TypeError("update requires a TerminalProfile")
+        # Copy private data
+        for k in other._io_data.keys():
+            if k in self._io_data:
+                self._io_data[k].update(other._io_data[k])
+            else:
+                self._io_data[k] = other._io_data[k]
+        return dict.update(self, other)
+
+    def __getitem__ (self, k):
+        if k not in self._valid_keys:
+            raise KeyError(self._bad_key(k))
+        return dict.__getitem__(self, k)
+
+    def __setitem__ (self, k, v):
+        if k not in self._valid_keys:
+            raise KeyError(self._bad_key(k))
+        return dict.__setitem__(self, k, v)
+
+    def __delitem__ (self, k):
+        if k not in self._valid_keys:
+            raise KeyError(self._bad_key(k))
+        return dict.__delitem__(self, k)
+    #}}}
+
+    def __str__ (self):
+        return "<TerminalProfile %s>" % self.name
+
+    def _bad_key (self, k):
+        return "Key '%s' is not a valid TerminalProfile key." % k
+
+#}}}
+
+
+#{{{ Shared handling helpers for import/export
+
+def p_err (str):
+    print >>sys.stderr, str
+
+def usage (error_msg):
+    p_err(error_msg)
+    p_err("Use --help for details.")
+    sys.exit(2)
+
+#}}}
+
+
+#{{{ IO base class
+
+class TerminalIOBase (object):
+    def profile_names (self):
+        raise NotImplementedError("TerminalIOBase#profile_names")
+
+    def profile_exists (self, name):
+        raise NotImplementedError("TerminalIOBase#profile_exists")
+
+    def read_profile (self, name=None):
+        raise NotImplementedError("TerminalIOBase#read_profile")
+
+    def write_profile (self, profile):
+        raise NotImplementedError("TerminalIOBase#write_profile")
+
+#}}}
+
+
+#{{{ IO: gnome-terminal + gconf
+
+#{{{ GConf value boxing/unboxing
 # (aka "the way it was meant to be in a dynamic language")
 # You can use get_$TYPE if you know _a priori_ what type the key you're getting
 # is. Otherwise gconf_unbox(client.get(...)) will do its best.
@@ -131,59 +393,10 @@ def gconf_box (v, is_pair=False): #{{{
         gv = _gconf_box_primitive(v)
     return gv
     #}}}
-#}}}1
+#}}}
 
 
-#{{{1 Color conversion
-
-_c24_re = re.compile('^#?' + (3 * '([0-9a-f]{2})') + '$', re.I)
-_c48_re = re.compile('^#?' + (3 * '([0-9a-f]{4})') + '$', re.I)
-
-def color24 (c48): #{{{
-    """Convert a 48-bit color to the closest possible 24-bit color."""
-
-    m = _c48_re.match(c48)
-    if m is None:
-        raise ValueError("Invalid 48-bit color '%s'" % c48)
-    c24 = '#'
-    for hexval in m.groups():
-        byte = round(int(hexval, 16)/256, 0)
-        if byte > 255:
-            byte = 255
-        elif byte < 0:
-            byte = 0
-        c24 += '%02x' % byte
-    return c24
-    #}}}
-
-def color48 (c24): #{{{
-    """Convert a 24-bit color to a 48-bit color."""
-
-    c24 = c24.lstrip('#')
-    if len(c24) != 6:
-        raise ValueError("24-bit color '%s' has unexpected length" % c24)
-    c48 = []
-    for b in range(3):
-        v = c24[2*b:2*b+2]
-        c48.append(v + v)
-    return '#' + (''.join(c48))
-    #}}}
-
-def is_color24 (c):
-    if isinstance(c, basestring) and _c24_re.match(c):
-        return True
-    return False
-
-def is_color48 (c):
-    if isinstance(c, basestring) and _c48_re.match(c):
-        return True
-    return False
-
-#}}}1
-
-
-#{{{1 Gnome-terminal GConf value handling
-# Possible future directions: PuttyIO, KonsoleIO
+#{{{ Gnome-terminal profile handling
 
 class MockGConf (object):
     def set (self, k, v):
@@ -199,7 +412,7 @@ class MockGConf (object):
     def _print (self, k, v):
         print "SET: %s = %s" % (k, repr(v))
 
-class GnomeTerminalIO (object):
+class GnomeTerminalIO (TerminalIOBase):
     """GConf backend reader/writer for gnome-terminal."""
 
     APP_ROOT = '/apps/gnome-terminal'
@@ -375,213 +588,38 @@ class GnomeTerminalIO (object):
         return ":".join(pal)
     #}}}
 
-#}}}1
-
-
-#{{{1 Theme file IO
-#
-# The file format is intentionally simple: a zip file containing a theme.ini
-# file, whose keys/values are exactly what TerminalProfile is.
-#
-# theme.ini, of course, is just an INI file like so...
-# [Termitheme1]
-# key=value
-# key=value
-#
-# This way, everything can be handled with zipfile and ConfigParser; and
-# when we get to figuring out where to unpack a background-image, the image
-# can be neatly added to the theme zip without changing the overall format.
-#
-# As such, this version is designed not to care about extra files in the zip
-# archive.
-#
-
-class ThemeFile (object):
-    filename = None
-
-    _autotype_defs = [
-        (re.compile(r'^t', re.I), lambda x: True),
-        (re.compile(r'^f', re.I), lambda x: False),
-        (re.compile(r'^\d+$'), lambda x: int(x, 10)),
-        (re.compile(r'^\d*\.\d+(?:e\d+)?$', re.I), lambda x: float(x)),
-    ]
-
-    def __init__ (self, filename, profile_class=None):
-        self.filename = filename
-        if not profile_class:
-            profile_class = TerminalProfile
-        self._profile_ctor = profile_class
-
-    def read_open (self):
-        """Return a ConfigParser associated with the theme file."""
-
-        # Open zipfile and validate expected theme.ini file found
-        zf = zipfile.ZipFile(self.filename, 'r') # can raise IOError
-        # Python2.5 doesn't have ZipFile.open(), and ConfigParser doesn't
-        # have any way to read directly from a string.
-        fp = StringIO.StringIO(zf.read('theme.ini')) # can raise KeyError
-        zf.close()
-
-        cp = ConfigParser.RawConfigParser()
-        cp.readfp(fp)
-        return cp
-
-    def read_profile (self, parser):
-        profile_class = self._profile_ctor
-        try:
-            name = parser.get('Termitheme1', 'name')
-            p = profile_class(name)
-        except ConfigParser.NoOptionError:
-            raise KeyError("Theme file does not have a 'name' key.")
-
-        for k, v in parser.items('Termitheme1'):
-            if k == 'name': # special key
-                continue
-
-            # the autotyper makes this O(M*N) but there's no other way to
-            # get booleans into gconf as real booleans
-            p[k] = self._autotype(v)
-
-        return p
-
-    def read (self):
-        """Read the theme file in self.filename and return a profile."""
-        return self.read_profile(self.read_open())
-
-    def write (self, profile):
-        """Write the profile into self.filename."""
-
-        lines = ["[Termitheme1]\n", "name = %s\n" % profile.name]
-        for k,v in profile.items():
-            lines.append("%s = %s\n" % (k, v))
-
-        info = zipfile.ZipInfo()
-        info.filename = 'theme.ini'
-        # whoever designed this api is ridiculous
-        now = datetime.datetime.fromtimestamp(time.time())
-        info.date_time = (now.year, now.month, now.day,
-                          now.hour, now.minute, now.second)
-        info.compress_type = zipfile.ZIP_DEFLATED
-        info.external_attr = 0644 << 16L # thank you bug 3394 (swarren)
-
-        if os.path.exists(self.filename):
-            raise ValueError("File '%s' exists." % self.filename)
-        zf = zipfile.ZipFile(self.filename, 'w')
-        zf.writestr(info, ''.join(lines))
-        zf.close()
-
-    def _autotype (self, val):
-        for re, fn in self._autotype_defs:
-            if re.match(val):
-                return fn(val)
-        return val
+#}}}
 
 #}}}
 
 
-#{{{1 Generic terminal profile data structure
-# This is smarter than a dict for future support of PuTTY/Konsole themes.
+#{{{ IO: PuTTY + Windows registry
 
-class TerminalProfile (dict):
-    """Generic terminal theme."""
-
-    PROFILE_KEY_NAMES = ["color%d" % i for i in range(16)]
-    PROFILE_KEY_NAMES.extend([
-        # common (plus color0 through color15)
-        'bgcolor',
-        'cursor_shape',
-        'font',
-        'fgcolor',
-        'fgbold',
-        # gnome-terminal
-        'force_font',
-        'allow_bold',
-        'use_fgbold',
-        # PuTTY's cursor color, etc.? Konsole?
-    ])
-
-    name = None
-
-    def __init__ (self, name):
-        self._valid_keys = set(self.PROFILE_KEY_NAMES)
-        self.name = name
-        self._io_data = {}
-
-    def is_valid_key (self, k):
-        """Return True if k is a TerminalProfile key."""
-        return k in self._valid_keys
-
-    @contextmanager
-    def ioslave (self, slave_name):
-        """Give a reference to a slave-private dict to a block.
-        
-        Example: with profile.ioslave("konsole") as d: d['foo']='bar'"""
-        if slave_name not in self._io_data:
-            self._io_data[slave_name] = {}
-        yield self._io_data[slave_name]
-
-    #{{{ Dictionary interface
-    def update (self, other):
-        """Copy other's dictionary keys and ioslave data into self."""
-
-        if not isinstance(other, TerminalProfile):
-            raise TypeError("update requires a TerminalProfile")
-        # Copy private data
-        for k in other._io_data.keys():
-            if k in self._io_data:
-                self._io_data[k].update(other._io_data[k])
-            else:
-                self._io_data[k] = other._io_data[k]
-        return dict.update(self, other)
-
-    def __getitem__ (self, k):
-        if k not in self._valid_keys:
-            raise KeyError(self._bad_key(k))
-        return dict.__getitem__(self, k)
-
-    def __setitem__ (self, k, v):
-        if k not in self._valid_keys:
-            raise KeyError(self._bad_key(k))
-        return dict.__setitem__(self, k, v)
-
-    def __delitem__ (self, k):
-        if k not in self._valid_keys:
-            raise KeyError(self._bad_key(k))
-        return dict.__delitem__(self, k)
-    #}}}
-
-    def __str__ (self):
-        return "<TerminalProfile %s>" % self.name
-
-    def _bad_key (self, k):
-        return "Key '%s' is not a valid TerminalProfile key." % k
-
-#}}}1
-
-
-#{{{1 Shared handling helpers for import/export
-
-def p_err (str):
-    print >>sys.stderr, str
-
-def usage (error_msg):
-    p_err(error_msg)
-    p_err("Use --help for details.")
-    sys.exit(2)
-
-#}}}1
-
-
-class PuttyIO:
+class PuttyWinIO (TerminalIOBase):
     pass
 
+#}}}
 
-#{{{1 Terminal switching code
+
+#{{{ Terminal switching code
 
 if gconf:
     _term_support['gnome'] = GnomeTerminalIO
+    _default_term = 'gnome'
 if _winreg:
-    _term_support['putty'] = PuttyIO
+    _term_support['putty'] = PuttyWinIO
+    _default_term = 'putty'
+
+
+def get_default_terminal ():
+    """Get the (platform-dependent) default terminal name."""
+
+    return _default_term
+
+def get_terminal_types ():
+    """Get all available terminal names for the current platform."""
+
+    return _term_support.keys()
 
 def get_terminal_io (termname, wrapper=None):
     """Get the IO class corresponding to termname.
@@ -600,5 +638,5 @@ def get_terminal_io (termname, wrapper=None):
     else:
         return ctor()
 
-#}}}1
+#}}}
 
